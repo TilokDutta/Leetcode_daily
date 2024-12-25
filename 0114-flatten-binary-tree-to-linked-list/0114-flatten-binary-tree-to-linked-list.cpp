9998f7cbeb4415f175c91489adb9dfae9947384c
TilokDutta <132099887+TilokDutta@users.noreply.github.com>
2024-12-25 14:33:20 +0530

    Time: 0 ms (100%), Space: 17.4 MB (44.72%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    vector<TreeNode*> preorderTraversal(TreeNode* root) {
-        stack<TreeNode*> st;
-        if(root != nullptr) st.push(root);
-        vector<TreeNode*> res;
-        while(!st.empty()){
-            TreeNode* temp = st.top();
-            st.pop();
-            res.push_back(temp);
-            if(temp->right) st.push(temp->right);
-            if(temp->left) st.push(temp->left);
-        }
-        return res;
-    }
     void flatten(TreeNode* root) {
-        vector<TreeNode*> res = preorderTraversal(root);
-        int n = res.size();
-        for(int i = 0; i < n-1; i++){
-            res[i]->right = res[i+1];
-            res[i]->left = nullptr;
+        if(root == nullptr){
+            return;
+        }
+        TreeNode* curr = root;
+        while(curr != nullptr){
+            if(curr->left != nullptr){
+                TreeNode* r = curr->right;
+                curr->right = curr->left;
+
+                TreeNode* pred = curr->left;
+                while(pred->right != nullptr){
+                    pred = pred->right;
+                }
+                pred->right = r;
+                curr = curr->left;
+            }else{
+                curr = curr->right;
+            }
+        }
+        TreeNode* temp = root;
+        while(temp->right != nullptr){
+            temp->left = nullptr;
+            temp = temp->right;
         }
     }
 };
