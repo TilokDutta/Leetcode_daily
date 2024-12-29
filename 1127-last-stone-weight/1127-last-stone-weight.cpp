bd94ad36f3a39fe00d6cae1e28dbf7a3e48e196a
TilokDutta <132099887+TilokDutta@users.noreply.github.com>
2024-12-30 01:58:51 +0530

    Time: 0 ms (100%), Space: 9.8 MB (54.26%) - LeetHub

---
+++ 1127-last-stone-weight/1127-last-stone-weight.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(int el : stones){
+            pq.push(el);
+        }
+        while(pq.size() > 1){
+            int first = pq.top();
+            pq.pop();
+            int second = pq.top();
+            pq.pop();
+            if(first - second > 0){
+                pq.push(first-second);
+            }
+        }
+        if(!pq.empty()){
+            return pq.top();
+        }
+        return 0;
+    }
+};
