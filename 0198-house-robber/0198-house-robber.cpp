e2b4d5f50791d15ae8338c9efe057dd05b1c757b
TilokDutta <132099887+TilokDutta@users.noreply.github.com>
2025-01-08 17:35:38 +0530

    Time: 0 ms (100%), Space: 10.6 MB (32.9%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    vector<int> dp;
-    int helper(vector<int>& nums, int ind){
+    // vector<int> dp;
+    // int helper(vector<int>& nums, int ind){
+    //     int n = nums.size();
+    //     if(ind == n-1) return nums[ind];
+    //     if(ind == n-2) return max(nums[ind],nums[ind+1]);
+    //     if(dp[ind] != -1) return dp[ind];
+    //     return dp[ind] = max((nums[ind]+helper(nums,ind+2)),(0+helper(nums,ind+1)));
+    // }
+    int fbu(vector<int>& nums){
         int n = nums.size();
-        if(ind == n-1) return nums[ind];
-        if(ind == n-2) return max(nums[ind],nums[ind+1]);
-        if(dp[ind] != -1) return dp[ind];
-        return dp[ind] = max((nums[ind]+helper(nums,ind+2)),(0+helper(nums,ind+1)));
+        if(n == 1) return nums[0];
+        vector<int> dp(n);
+        dp[n-1] = nums[n-1];
+        dp[n-2] = max(nums[n-2],nums[n-1]);
+        for(int i = n-3; i>=0; i--){
+             dp[i] = max(nums[i] + dp[i+2], dp[i+1]);
+        }
+        return dp[0];
     }
     int rob(vector<int>& nums) {
-        dp.clear();
-        dp.resize(105,-1);
-        int ans = helper(nums,0);
+        // dp.clear();
+        // dp.resize(105,-1);
+        int ans = fbu(nums);
         return ans;
     }
 };
