0b754ce1f483c2b59650290abf9d4d6f51ba43b6
TilokDutta <132099887+TilokDutta@users.noreply.github.com>
2025-07-03 14:01:23 +0530

    Time: 1 ms (82.81%), Space: 17.9 MB (89.8%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        queue<int> q;
-        vector<vector<int>> adj(numCourses);
+    vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
+        vector<vector<int>> prereq(numCourses);
         vector<int> inDegree(numCourses,0);
-        for(int i = 0; i < prerequisites.size(); i++){
-            int course = prerequisites[i][0];
-            int preReq = prerequisites[i][1];
-
-            adj[preReq].push_back(course);
-            inDegree[course]++;
+        for(int i = 0 ; i < pre.size(); i++){
+            prereq[pre[i][1]].push_back(pre[i][0]);
+            inDegree[pre[i][0]]++;
         }
-        for(int i = 0; i < numCourses; i++){
-            if(inDegree[i] == 0){
+        queue<int> q;
+        for(int i = 0; i < inDegree.size(); i++){
+            if(inDegree[i] == 0) {
                 q.push(i);
             }
         }
-        vector<int> topo;
+        vector<int> ans;
         while(!q.empty()){
-            int pre = q.front();
+            int course = q.front();
+            ans.push_back(course);
             q.pop();
-            topo.push_back(pre);
-            for(auto cour : adj[pre]){
-                inDegree[cour]--;
-                if(inDegree[cour] == 0){
-                    q.push(cour);
+            for(auto it : prereq[course]){
+                inDegree[it] --;
+                if(inDegree[it] == 0){
+                    q.push(it);
                 }
             }
         }
-        if(topo.size() == numCourses) return topo;
+        if(ans.size() == numCourses){
+            return ans;
+        }
         return {};
     }
 };
