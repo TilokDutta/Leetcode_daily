5e678e0e603352cbafae67f119c94fbd4768fb5f
TilokDutta <132099887+TilokDutta@users.noreply.github.com>
2025-07-10 03:24:36 +0530

    Time: 3 ms (67.02%), Space: 8.4 MB (11.09%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int helper(int n, vector<int>& v){
-        if(n == 0 || n == 1) return n;
-        if(v[n] != -1) return v[n];
-        int ans = helper(n-1,v) + helper(n-2,v);
-        v[n] = ans;
-        return ans;
-    }
     int fib(int n) {
-        vector<int> v(n+1,-1);
-        return helper(n,v);
+        if(n == 0 || n == 1) return n;
+        vector<int> dp(n+1,-1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
