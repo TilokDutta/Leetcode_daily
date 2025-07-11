a9614c0a61a51a7ec86c5c80709c3fe507e633a7
TilokDutta <132099887+TilokDutta@users.noreply.github.com>
2025-07-11 15:41:48 +0530

    Time: 0 ms (100%), Space: 10.6 MB (46.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,13 +4,16 @@ public:
         int n = nums.size();
         if(n == 1) return nums[0];
         vector<int> dp(n,-1);
-        dp[0] = nums[0];
+        int prev1 = nums[0];
+        int prev2 = nums[0];
         for(int i = 1; i < n; i++){
             int pick = nums[i];
-            if(i != 1 ) pick += dp[i-2];
-            int notPick = 0 + dp[i-1];
-            dp[i] = max(pick,notPick);
+            if(i != 1 ) pick += prev2;
+            int notPick = 0 + prev1;
+            int curr = max(pick,notPick);
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n-1];
+        return prev1;
     }
 };
