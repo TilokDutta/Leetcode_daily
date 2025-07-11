c8bff313ec52f061271c94c43f51ae64ef8ae94b
TilokDutta <132099887+TilokDutta@users.noreply.github.com>
2025-07-11 17:08:31 +0530

    Time: 0 ms (100%), Space: 10 MB (97.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,7 +3,6 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return nums[0];
-        vector<int> dp(n,-1);
         int prev1 = nums[0];
         int prev2 = nums[0];
         for(int i = 1; i < n; i++){
