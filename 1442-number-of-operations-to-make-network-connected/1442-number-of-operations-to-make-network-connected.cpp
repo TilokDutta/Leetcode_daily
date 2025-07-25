cdf6c5425b1a8fe025e11cd189edfa247574dd92
TilokDutta <132099887+TilokDutta@users.noreply.github.com>
2025-07-26 03:45:01 +0530

    Time: 3 ms (87.64%), Space: 42.1 MB (85.37%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -1,70 +1,45 @@
-class DisjointSet{
-    vector<int> rank,parent,size;
-public:
-    DisjointSet(int n){
-        size.resize(n+1,1);
-        rank.resize(n+1,0);
-        parent.resize(n+1,0);
-        for(int i = 0; i < n; i++){
-            parent[i] = i;
-        } 
-    }
-    int findUParent(int node){
-        if(node == parent[node]){
-            return node;
-        }
-        return parent[node] = findUParent(parent[node]);
-    }
-    void unionByRank(int u, int v){
-        int ulP_u = findUParent(u);
-        int ulp_v = findUParent(v);
-        if(ulP_u == ulp_v) return;
-        if(rank[ulP_u] < rank[ulp_v]){
-            parent[ulP_u] = ulp_v;
-        }else if(rank[ulP_u] > rank[ulp_v]){
-            parent[ulp_v] = ulP_u;
-        }else{
-            parent[ulp_v] = ulP_u;
-            rank[ulP_u]++;
-        }
-    }
-    void unionBySize(int u, int v){
-        int ulP_u = findUParent(u);
-        int ulp_v = findUParent(v);
-        if(ulP_u == ulp_v) return;
-        if(size[ulP_u] < size[ulp_v]){
-            parent[ulP_u] = ulp_v;
-            size[ulp_v] += size[ulP_u];
-        }else{
-            parent[ulp_v] = ulP_u;
-            size[ulP_u] += size[ulp_v];
-        }
-    }
-};
-
 class Solution {
 public:
+    vector<int> parent;
+    vector<int> rank;
+    int find(int i){
+        if(parent[i] == i){
+            return i;
+        }
+        return parent[i] = find(parent[i]);
+    }
+    void unionSet(int x, int y){
+        int parent_x = find(x);
+        int parent_y = find(y);
+        if(parent_x == parent_y){
+            return;
+        }
+        if(rank[parent_x] > rank[parent_y]){
+            parent[parent_y] = parent_x;
+        }else if(rank[parent_x] < rank[parent_y]){
+            parent[parent_x] = parent_y;
+        }else{
+            parent[parent_y] = parent_x;
+            rank[parent_x]++;
+        }
+    }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        DisjointSet ds(n);
-        int extraEdges = 0;
-        for(auto it : connections){
-            int u = it[0];
-            int v = it[1];
-            if(ds.findUParent(u) != ds.findUParent(v)){
-                ds.unionBySize(u,v);
-            }else{
-                extraEdges++;
-            }
-        }
-        int components = 0;
+        int edgeCount = connections.size();
+        if(edgeCount < n-1) return -1;
+        parent.resize(n);
+        rank.resize(n,0);
         for(int i = 0; i < n; i++){
-            if(ds.findUParent(i) == i){
-                components++;
+            parent[i] = i;
+        }
+        int count = n;
+        for(int i = 0; i < edgeCount; i++){
+            int parent_x = find(connections[i][0]);
+            int parent_y = find(connections[i][1]);
+            if(parent_x != parent_y){
+                unionSet(connections[i][0],connections[i][1]);
+                count--;
             }
         }
-        if(extraEdges >= components-1){
-            return components-1;
-        }
-        return -1;
+        return count-1;
     }
 };
