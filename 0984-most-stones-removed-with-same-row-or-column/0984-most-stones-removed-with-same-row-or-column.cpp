f018dd61eacc2c5c04b063f25a5aedd9db95f735
TilokDutta <132099887+TilokDutta@users.noreply.github.com>
2025-07-28 23:50:19 +0530

    Time: 111 ms (5.06%), Space: 18 MB (97.06%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -1,78 +1,25 @@
-class DisjointSet{
-    vector<int> rank,parent,size;
-public:
-    DisjointSet(int n){
-        size.resize(n+1,1);
-        rank.resize(n+1,0);
-        parent.resize(n+1,0);
-        for(int i = 0; i < n; i++){
-            parent[i] = i;
-        } 
-    }
-    int findUParent(int node){
-        if(node == parent[node]){
-            return node;
-        }
-        return parent[node] = findUParent(parent[node]);
-    }
-    void unionByRank(int u, int v){
-        int ulP_u = findUParent(u);
-        int ulp_v = findUParent(v);
-        if(ulP_u == ulp_v) return;
-        if(rank[ulP_u] < rank[ulp_v]){
-            parent[ulP_u] = ulp_v;
-        }else if(rank[ulP_u] > rank[ulp_v]){
-            parent[ulp_v] = ulP_u;
-        }else{
-            parent[ulp_v] = ulP_u;
-            rank[ulP_u]++;
-        }
-    }
-    void unionBySize(int u, int v){
-        int ulP_u = findUParent(u);
-        int ulp_v = findUParent(v);
-        if(ulP_u == ulp_v) return;
-        if(size[ulP_u] < size[ulp_v]){
-            parent[ulP_u] = ulp_v;
-            size[ulp_v] += size[ulP_u];
-        }else{
-            parent[ulp_v] = ulP_u;
-            size[ulP_u] += size[ulp_v];
-        }
-    }
-};
-
-
 class Solution {
 public:
+    void dfs(vector<bool> &vis, int &index, vector<vector<int>>& stones){
+        vis[index] = 1;
+        for(int j = 0; j < stones.size(); j++){
+            int r = stones[j][0];
+            int c = stones[j][1];
+            if((r == stones[index][0] || c == stones[index][1]) && vis[j] == 0){
+                dfs(vis,j,stones);
+            }
+        }
+    }
     int removeStones(vector<vector<int>>& stones) {
-        vector<vector<int>> edges;
-
         int n = stones.size();
+        vector<bool> vis(n,0);
+        int count = 0;
         for(int i = 0; i < n; i++){
-            int x = stones[i][0];
-            int y = stones[i][1];
-            for(int j = i+1; j < n; j++){
-                if(stones[j][0] == x || stones[j][1] == y){
-                    edges.push_back({i,j});
-                }
+            if(vis[i] == 0){
+                dfs(vis,i,stones);
+                count++;
             }
         }
-
-        DisjointSet ds(n);
-        for(auto it : edges){
-            int u = it[0];
-            int v = it[1];
-            if(ds.findUParent(u) != ds.findUParent(v)){
-                ds.unionBySize(u,v);
-            }
-        }
-        int unConnected = 0;
-        for(int i = 0 ;i < n; i++){
-            if(ds.findUParent(i) == i){
-                unConnected++;
-            }
-        }
-        return n == 1 ? 0 : n-unConnected;
+        return n-count;
     }
 };
