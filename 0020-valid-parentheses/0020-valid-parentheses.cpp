e414637192bdfb65defe4f678b569e2b9e6161b0
TilokDutta <132099887+TilokDutta@users.noreply.github.com>
2025-08-28 18:36:06 +0530

    Time: 0 ms (100%), Space: 10.1 MB (5.47%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
         int n = s.size();
+        stack<int> st;
         for(int i = 0; i < n; i++){
-            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
                 st.push(s[i]);
-            }else if(s[i] == ')' || s[i] == ']' || s[i] == '}'){
+            }else{                
                 if(st.empty()) return false;
-                char last = st.top();
-                st.pop();
-                if((s[i] == ')' && last != '(') || (s[i] == '}' && last != '{') || (s[i] == ']' && last != '[')){
-                    return false;    
+                char last = st.top(); st.pop();
+                if((s[i] == '}' && last != '{') || (s[i] == ']' && last != '[') || (s[i] == ')' && last != '(')){
+                    return false;
                 }
             }
         }
-        if(!st.empty()){
-            return false;
-        }
+        if(!st.empty()) return false;
         return true;
     }
 };
