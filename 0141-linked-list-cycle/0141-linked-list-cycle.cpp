97c1777506d4fa2833fe9feeb69bcc348cecd590
TilokDutta <132099887+TilokDutta@users.noreply.github.com>
2025-08-31 17:44:15 +0530

    Time: 9 ms (47.85%), Space: 11.9 MB (23.21%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -10,6 +10,7 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         if(head == nullptr || head->next == nullptr) return false;
+        ListNode* res;
         ListNode* slow = head;
         ListNode* fast = head->next;
         while(slow != fast){
