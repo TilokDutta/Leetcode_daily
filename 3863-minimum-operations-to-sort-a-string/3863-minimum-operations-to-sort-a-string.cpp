8fedef8c88bea1f5d127604a5ef95945a431cb03
TilokDutta <132099887+TilokDutta@users.noreply.github.com>
2026-03-14 18:59:10 +0530

    Time: 15 ms (81.54%), Space: 27.5 MB (85.99%) - LeetHub

---
+++ 3863-minimum-operations-to-sort-a-string/3863-minimum-operations-to-sort-a-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        if(is_sorted(s.begin(),s.end())){
+            return 0;
+        }
+        if(s.size() == 2) return -1;
+        char mini = 'z';
+        char maxi = 'a';
+        for(int i = 0; i < s.size(); i++){
+            if(s[i]  <= mini){
+                mini = s[i];
+            }
+            if(s[i] >= maxi){
+                maxi = s[i];
+            }
+        }
+        if(mini == s[0] || maxi == s[s.size() -1]){
+            return 1;
+        }
+
+        if(count(s.begin(),s.end(),mini) > 1 || count(s.begin(),s.end(),maxi) > 1){
+            return 2;
+        }
+        if(is_sorted(s.begin(), s.end(), greater<char>()) || (s.back() == mini && s.front() == maxi)){
+            return 3;
+        }
+        return 2;
+    }
+};
