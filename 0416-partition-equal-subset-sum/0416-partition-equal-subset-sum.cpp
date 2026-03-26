7d7a1d26ebabce772f2dc8ebb57d1fbb182250d3
TilokDutta <132099887+TilokDutta@users.noreply.github.com>
2026-03-27 01:04:46 +0530

    Time: 158 ms (28.75%), Space: 204.1 MB (8.46%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    bool solve(vector<int> &nums, long long sum,int i,vector<vector<int>>& dp){
-        if(sum == 0) return true;
-        if(i == 0) return (nums[0] == sum);
-        if(dp[i][sum] != -1) return dp[i][sum];
-        bool notTake = solve(nums,sum,i-1,dp);
-        bool take = false;
-        if(sum >= nums[i]){
-            take = solve(nums,sum-nums[i],i-1,dp);
+    int f(int ind, int target, vector<int>& arr,vector<vector<int>> &dp){
+        if(target == 0) return dp[ind][target] = 1;
+        if(ind == 0){
+            if(arr[ind] == target) return dp[ind][target] = 1;
+            else return dp[ind][target] = 0;
         }
-        return dp[i][sum] = notTake | take;
+        if(dp[ind][target] != -1) return dp[ind][target];
+        bool notTake = f(ind-1,target,arr,dp);
+        bool take = 0;
+        if(target >= arr[ind]){
+            take = f(ind-1,target-arr[ind],arr,dp);
+            
+        }
+        return dp[ind][target] = take || notTake;
     }
     bool canPartition(vector<int>& nums) {
-        long long sum = 0;
         int n = nums.size();
-        for(int i = 0; i < n; i++){
-            sum += nums[i];
-        }
-        vector<vector<int>> dp(n+1,vector<int>(sum/2+1,-1));
-        if(sum % 2 != 0) return false;
-        return solve(nums,sum/2,n-1,dp);
+        int target = accumulate(nums.begin(),nums.end(),0);
+        if(target % 2 != 0) return false;
+        vector<vector<int>> dp(n,vector<int> (target+1,-1));
+        return f(n-1,target/2,nums,dp);
     }
 };
