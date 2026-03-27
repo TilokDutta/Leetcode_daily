03aa9a3752e7593948d47579daab297506aa984d
TilokDutta <132099887+TilokDutta@users.noreply.github.com>
2026-03-27 21:30:27 +0530

    Time: 43 ms (40.79%), Space: 49.5 MB (24.36%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    int f(int x,vector<int>& coins,vector<int>& dp){
-        if(x == 0) return 0;
-        if(dp[x] != -2) return dp[x];
-        int ans = INT_MAX; 
-        for(int i = 0;i < coins.size();i++){
-            if(x < coins[i]) continue;
-            ans = min(ans,f(x-coins[i],coins,dp));
+    int f(int ind,int target, vector<int> &coins, vector<vector<int>> &dp){
+        if(target == 0) return dp[ind][target] = 0;
+        if(ind  == 0){
+            if(target % coins[ind] == 0) return dp[ind][target] = target/coins[ind];
+            else return 1e9;
         }
-        if( ans == INT_MAX) return dp[x] = INT_MAX;
-        return dp[x] = ans + 1;
+        if(dp[ind][target] != -1) return dp[ind][target];
+        int notTake = 0 + f(ind-1,target,coins,dp);
+        int take = INT_MAX;
+        if(target >= coins[ind]){
+            take = 1 + f(ind,target-coins[ind],coins,dp);
+        }
+        return dp[ind][target] = min(notTake, take);
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(1000006,-2);
-        int res = f(amount,coins,dp);
-        if(res == INT_MAX){
-            return -1;
-        }
-        return res;
+        int n = coins.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        if(amount == 0) return 0;
+        int ans = f(n-1,amount,coins,dp);
+        return ans >= 1e9 ? -1 : ans;
     }
 };
