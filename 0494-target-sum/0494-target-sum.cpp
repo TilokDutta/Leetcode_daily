09a45bf76ae099fdfbed6dae0e7abee6bdb54746
TilokDutta <132099887+TilokDutta@users.noreply.github.com>
2026-03-28 00:11:03 +0530

    Time: 419 ms (24.28%), Space: 11.7 MB (84.01%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    int f(int ind, int target, vector<int>& arr, vector<vector<int>>& dp){
+    int f(int ind, int target, int sum, vector<int> &nums){
         if(ind == 0){
-            if(target == 0 && arr[0] == 0) return 2;
-            if(target == 0) return 1;
-            if(arr[0] == target) return 1;
-            return 0;
+            int count = 0;
+            if(sum + nums[0] == target) count++;  
+            if(sum - nums[0] == target) count++;  
+            return count;
         }
-        if(dp[ind][target] != -1) return dp[ind][target];
-        int notTake = f(ind-1, target, arr, dp);
-        int take = 0;
-        if(target >= arr[ind])
-            take = f(ind-1, target - arr[ind], arr, dp);
-        return dp[ind][target] = take + notTake;
+        int sub = f(ind-1,target,sum-nums[ind],nums);
+        int add = f(ind-1,target,sum+nums[ind],nums);
+        return sub + add;
     }
-
     int findTargetSumWays(vector<int>& nums, int target) {
-        int total = accumulate(nums.begin(), nums.end(), 0);
-        // assigning + to s1 subset, - to s2 subset
-        // s1 - s2 = target, s1 + s2 = total
-        // => s1 = (total + target) / 2
-        if((total + target) < 0 || (total + target) % 2 != 0) return 0;
-        int s1 = (total + target) / 2;
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(s1+1, -1));
-        return f(n-1, s1, nums, dp);
+        int n= nums.size();
+        if(n == 1 && nums[0] == 0 && target == 0) return 2;
+        return f(nums.size() - 1, target,0,nums);
     }
 };
